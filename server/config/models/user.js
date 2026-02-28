e53edfc823ec35384862b703feadd2c0844ba2df
saad-aziz1 <saadaziz10000@gmail.com>
2026-02-28 14:29:38 +0500

    feat: create User model with password hashing and security layers

---
+++ server/config/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide your name"],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+    select: false
+  }
+}, { timestamps: true });
+
+// PasswordEncrypt
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// PasswordMatch 
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
